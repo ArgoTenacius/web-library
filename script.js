428cbd2cca34806c24a8072e2308e6c4334ec4d6
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-02-23 20:56:29 -0300

    Script remade, console log book array working.

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
-let myLibrary = [];
 const book_area = document.getElementById("book_area")
 
-function Book(title, author, pages, finished){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.finished = finished
+class Book{
+    constructor(title, author, pages, readed){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.readed = readed
+    }
 }
 
-function addBookToLibrary(title, author, pages, finished){
-    const newBook = new Book(title, author, pages, finished)
-    myLibrary.push(newBook)
-    console.log(myLibrary)
+class Library{
+    constructor(){
+        this.books = []
+    }
+
+    addBook(newBook) { 
+        this.books.push(newBook) 
+        console.log(this.books) 
+    }
 }
 
-function newBook(){
-    const __newBook = document.createElement("div")
-    const __newBook_config = document.createElement("form")
-    const __title_input = document.createElement("input")
-    const __author_input = document.createElement("input")
-    const __pages_input = document.createElement("input")
-    const __readed = document.createElement("div")
-    const __readed_checkbox = document.createElement("input")
-    const __submit = document.createElement("button")
+const library = new Library()
 
-    __newBook.className = "newbook"
-    __newBook_config.className = "newbook-config"
-    __submit.className="book-button submit"
+const newBook = new Book("Percy Jackson", "Rick Riordan", "264", false)
 
-    __title_input.placeholder = "Title"
-    __author_input.placeholder = "Author"
-    __pages_input.placeholder = "Pages:"
-    __submit.innerText = "Submit"
+library.addBook(newBook)
 
-    __readed.className = "readed"
-    __readed.innerText = "Already readed ?"
-    __readed_checkbox.type = "checkbox"
-    __readed.appendChild(__readed_checkbox)
+createBook = (book) => {
+    const bookCard = document.createElement("div")
+    const title = document.createElement("h1")
+    const author = document.createElement("h3")
+    const pages = document.createElement("h4")
+    const readBtn = document.createElement("button")
+    const removeBtn = document.createElement("button")
 
-    __newBook.appendChild(__newBook_config)
-    __newBook_config.append(__title_input, __author_input, __pages_input, __readed, __submit)
-    book_area.appendChild(__newBook)
+    bookCard.className = "book"
+    title.className = "book-title"
+    author.className = "book-author"
+    pages.className = "book-author"
+    readBtn.classList = "book-button read"
+    removeBtn.classList = "book-button remove"
+
+    title.textContent = book.title
+    author.textContent = book.author
+    pages.textContent = book.pages
+    removeBtn.textContent = "remove"
+
+    bookCard.append(title, author, pages, readBtn, removeBtn)
+    book_area.appendChild(book)
 }
-
-addBookToLibrary("Percy Jackson", "Rick Riordan", 250, true)
